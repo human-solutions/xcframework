9234f5aa9e09dde4e3809e7070b026bab514e67e
hakesson <henrik@akesson.mobi>
2023-02-13 18:09:23 +0100

    Remove Windows path cleaning (this is macOS only)

--- src/ext/mod.rs
+++ src/ext/mod.rs
@@ -1,7 +1,5 @@
-mod cargo;
 mod path;
 mod triple;
 
-pub use cargo::MetadataExt;
 pub use path::PathBufExt;
 pub use triple::TripleExt;
--- src/ext/path.rs
+++ src/ext/path.rs
@@ -2,20 +2,10 @@ use anyhow::{Context, Result};
 use camino::Utf8PathBuf;
 
 pub trait PathBufExt {
-    /// cleaning the unc (illegible \\?\) start of windows paths. See dunce crate.
-    fn clean_windows_path(&mut self);
-
     fn resolve_home_dir(self) -> Result<Utf8PathBuf>;
 }
 
 impl PathBufExt for Utf8PathBuf {
-    fn clean_windows_path(&mut self) {
-        if cfg!(windows) {
-            let cleaned = dunce::simplified(self.as_ref());
-            *self = Utf8PathBuf::from_path_buf(cleaned.to_path_buf()).unwrap();
-        }
-    }
-
     fn resolve_home_dir(self) -> Result<Utf8PathBuf> {
         if self.starts_with("~") {
             let home = std::env::var("HOME").context("Could not resolve $HOME")?;
