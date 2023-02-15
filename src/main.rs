bf0751fad40660636af7e1f089ba0131d6af871b
hakesson <henrik@akesson.mobi>
2023-02-15 08:36:42 +0100

    Fix for clap to parse when running `cargo xcframework`

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let mut args: Vec<String> = env::args().collect();
     // when running as cargo leptos, the second argument is "leptos" which
     // clap doesn't expect
-    if args.get(1).map(|a| a == "uniffi-package").unwrap_or(false) {
+    if args.get(1).map(|a| a == "xcframework").unwrap_or(false) {
         args.remove(1);
     }
 
