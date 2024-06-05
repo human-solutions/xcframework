bdd52e37838e0c981ca44154fc7ccac4824197ee
Binlogo <binboy@live.com>
2024-06-05 22:28:39 +0800

    chore: workaround fix tests

--- tests/test.rs
+++ tests/test.rs
@@ -20,6 +20,13 @@ fn test_hello() {
 
     // FIXME: if needed targets missing, it will block running the test by interactive prompt
     // WORKAROUND: add the targets fisrt
+    rustup_configurator::target::install(&vec![
+        "aarch64-apple-darwin".to_owned(),
+        "aarch64-apple-ios".to_owned(),
+        "aarch64-apple-ios-sim".to_owned(),
+        "x86_64-apple-darwin".to_owned(),
+        "x86_64-apple-ios".to_owned(),
+    ]).unwrap();
 
     let produced = xcframework::build(cli).unwrap();
     assert!(produced.is_zipped);
